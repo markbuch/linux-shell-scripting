8d778dedfeffd8d583efcd100de503ae58c79bf6
Mark Buchanan <markbuch24@outlook.com>
2019-11-13 19:47:42 -0500

    Completed more examples.

---
+++ read3.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+COUNT=1
+
+while IFS='' read -r LINE
+do
+    if [ $COUNT -le "3" ]
+    then
+        echo "LINE $COUNT": $LINE
+    else
+        break
+    fi
+    ((COUNT++))
+done < "$1"
+
+exit 0
